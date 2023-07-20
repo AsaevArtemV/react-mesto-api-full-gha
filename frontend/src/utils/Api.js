70180c14b6445e943d2047015159b94d445664df
asaev <ArtemAV13@yandex.ru>
2023-07-21 00:20:00 +0300

    Изменил код файла api.js

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,108 +1,131 @@
 class Api {
-  constructor({ baseUrl, headers }) {
-    this._baseUrl = baseUrl;
-    this._headers = headers;
-  }
-
-  //Проверяет на ошибку
-  _checkResponse(res) {
-    return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
-  }
-
-//Загрузка информации о пользователе с сервера
-  getUserInfo() {
-    return fetch(`${this._baseUrl}/users/me`, {
-      headers: this._headers
-    })
-    .then(res => this._checkResponse(res))
-  }
-
-//Загрузка карточек с сервера
-  getInitialCards() {
-    return fetch(`${this._baseUrl}/cards`, {
-      headers: this._headers
-    })
-    .then(res => this._checkResponse(res))
-  }
-
-//Устанавливает новые имя и профессию текущего пользователя
-  setUserInfo({ name, about }) {
-    return fetch(`${this._baseUrl}/users/me`, {
-      method: 'PATCH',
-      headers: this._headers,
-      body: JSON.stringify({
-        name: name,
-        about: about,
-      }),
-    })
-    .then(res => this._checkResponse(res))
-  }
-
-//Добавление новой карточки
-  addNewCard({ name, link }) {
-    return fetch(`${this._baseUrl}/cards`, {
-      method: 'POST',
-      headers: this._headers,
-      body: JSON.stringify({
-        name: name,
-        link: link,
-      }),
-    })
-    .then(res => this._checkResponse(res))
-  }
-
-// добавить лайк карточки
-  addLike(id) {
-    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
-      method: 'PUT',
-      headers: this._headers,
-    })
-    .then(res => this._checkResponse(res))
-  }
-
-// удалить лайк карточки
-  deleteLike(id) {
-    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
-      method: 'DELETE',
-      headers: this._headers,
-    })
-      .then(res => this._checkResponse(res))
-  }
-
-  changeLikeCardStatus(id, isLiked) {
-    if (!isLiked) {
-      return api.addLike(id);
+    constructor(options) {
+      this._baseUrl = options.baseUrl;
     }
-    return api.deleteLike(id);
-  }
+  
+    //Проверяет на ошибку
+    _checkResponse(res) {
+      if (res.ok) {
+        return Promise.resolve(res.json());
+      }
+  
+      //reject promise
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+  
+    //Устанавливает новые имя и профессию текущего пользователя
+    async setUserInfo(data) {
+      const response = await fetch(`${this._baseUrl}/users/me`, {
+        method: "PATCH",
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('JWT')}`,
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({
+          name: data.name,
+          about: data.about,
+        }),
+      });
+      return this._checkResponse(response);
+    }
+  
+    //Загрузка информации о пользователе с сервера
+    async getUserInfo() {
+      const response = await fetch(`${this._baseUrl}/users/me`, {
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('JWT')}`,
+            'Content-Type': 'application/json'
+          },
+      });
+      return this._checkResponse(response);
+    }
+  
+    //Загрузка карточек с сервера
+    async getInitialCards() {
+      const response = await fetch(`${this._baseUrl}/cards`, {
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('JWT')}`,
+            'Content-Type': 'application/json'
+          },
+      });
+      return this._checkResponse(response);
+    }
+  
+    //Добавление новой карточки
+    async addNewCard(data) {
+      const response = await fetch(`${this._baseUrl}/cards`, {
+        method: "POST",
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('JWT')}`,
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(data),
+      });
+      return this._checkResponse(response);
+    }
+  
+    //Удаление карточки
+    async deleteCard(cardId) {
+      const response = await fetch(`${this._baseUrl}/cards/${cardId}`, {
+        method: "DELETE",
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('JWT')}`,
+            'Content-Type': 'application/json'
+          },
+      });
+      return this._checkResponse(response);
+    }
+  
+    //добавить лайк карточки
+    async addLike(cardId) {
+      const response = await fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+        method: "PUT",
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('JWT')}`,
+            'Content-Type': 'application/json'
+          },
+      });
+      return this._checkResponse(response);
+    }
+  
+    //удалить лайк карточки
+    async deleteLike(cardId) {
+      const response = await fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+        method: "DELETE",
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('JWT')}`,
+            'Content-Type': 'application/json'
+          },
+      });
+      return this._checkResponse(response);
+    }
 
-//Удаление карточки
-  deleteCard(id) {
-    return fetch(`${this._baseUrl}/cards/${id}`, {
-      method: 'DELETE',
-      headers: this._headers,
-    })
-      .then(res => this._checkResponse(res))
+    changeLikeCardStatus(cardId, isLiked) {
+        if (!isLiked) {
+          return api.addLike(cardId);
+        }
+        return api.deleteLike(cardId);
+      }
+  
+    //avatar update avatar
+    async setUserAvatar(data) {
+      const response = await fetch(`${this._baseUrl}/users/me/avatar`, {
+        method: "PATCH",
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('JWT')}`,
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({
+          avatar: data.avatar,
+        }),
+      });
+      return this._checkResponse(response);
+    }
   }
-
-//Обновление аватара пользователя
-  setUserAvatar({ link }) {
-    return fetch(`${this._baseUrl}/users/me/avatar`, {
-      method: 'PATCH',
-      headers: this._headers,
-      body: JSON.stringify({
-        avatar: link,
-      }),
-    })
-    .then(res => this._checkResponse(res))
-  }
-}
-
-//Настройки для подключения к серверу
-export const api = new Api({
-  baseUrl: 'https://mesto.arteva.nomoredomains.xyz',
-  headers: {
-    authorization: `Bearer ${localStorage.getItem('JWT')}`,
-    'Content-Type': 'application/json'
-  }
-});
+  
+  //connect api
+  const api = new Api({
+    baseUrl: "https://mesto.arteva.nomoredomains.xyz",
+  });
+  
+  export default api;
